e1ff3abf2000395851dbb9cc17105ff4664f47d7
thery <Laurent.Thery@inria.fr>
2024-02-14 03:40:52 +0100

    some progress

--- ostro.v
+++ ostro.v
@@ -1,8 +1,8 @@
 Require Import Reals Psatz.
 From mathcomp Require Import ssreflect ssrbool ssrnat eqtype ssrfun. 
 From mathcomp Require Import fintype bigop seq.
-Require Import Zpower moreR.
-From rfac Require Import rfrac.
+Require Import Zpower.
+Require Import moreR moreZ rfrac.
 
 Open Scope R_scope.
 
@@ -19,12 +19,31 @@ Fixpoint mko_list (r : R) (n : nat) (v : Z) : list Z :=
   if n is n1.+1 then
     (v / 'q[r]_n)%Z :: mko_list r n1 (v mod 'q[r]_n)%Z
   else nil.
-  
-Definition ostro r n i := 
-  nth 0%Z (mko_list r (bostro r n) (Z.of_nat n)) (bostro r n - i).
 
-Local Notation " 'o[ r , n ]_ i" := (ostro r n i) 
-  (at level 10, format " ''o[' r ,  n ]_ i").
+Lemma mko_listS (r : R) (n : nat) (v : Z) :
+  mko_list r n.+1 v = 
+    (v / 'q[r]_n.+1)%Z :: mko_list r n (v mod 'q[r]_n.+1)%Z.
+Proof. by []. Qed.
+
+Lemma size_mko_list r n v : size (mko_list r n v) = n.
+Proof. by elim: n v => //= n IH v; rewrite IH. Qed.
+
+Lemma big_mko_list r n v : 
+  let l := mko_list r n v in 
+  (0 < n)%nat -> 
+  v = \big[Zplus/0%Z]_(i < n.+1) (nth 0%Z l (n - i) * 'q[r]_i)%Z.
+Proof.
+elim: n v => // [] [|n] IH v l _.
+  by rewrite !big_ord_recr big_ord0 denom_1 /= Z.div_1_r; lia.
+rewrite /l mko_listS big_ord_recr /= subnn /=.
+rewrite [LHS](Z.div_mod v ('q[r]_n.+2)); last first.
+  by have := denom_spos n.+1 r; lia.
+rewrite Zplus_comm; congr (_ + _)%Z; last by lia.
+rewrite [LHS]IH //.
+apply: eq_bigr => /= u _; congr (_ * _)%Z.
+suff -> : (n.+2 - u = (n.+1 - u).+1)%nat by [].
+by rewrite subSn // -ltnS.
+Qed.
 
 Lemma bostroP r n : 
   irrational r -> 
@@ -46,3 +65,17 @@ apply: Z.le_lt_trans (irrational_denom_lt _ _ iR).
 by apply: denom_le.
 Qed.
 
+Definition ostro r n i := 
+  nth 0%Z (mko_list r (bostro r n) (Z.of_nat n)) (bostro r n - i).
+
+Local Notation " 'o[ r , n ]_ i" := (ostro r n i) 
+  (at level 10, format " ''o[' r ,  n ]_ i").
+
+Lemma big_ostro r n : 
+  irrational r -> 
+  (Z.of_nat n = \big[Zplus/0%Z]_(i < (bostro r n).+1) ('o[r, n]_i * 'q[r]_i))%Z.
+Proof.
+move=> rI; apply: big_mko_list.
+case: bostro (bostroP r n rI) => //.
+by rewrite denom_0; lia.
+Qed.
